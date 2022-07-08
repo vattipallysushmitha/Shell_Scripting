2daac44a94d19256814a10440c7c7bc67feb139a
vattipallysushmitha <vengalreddysushmitha@gmail.com>
2022-07-08 16:04:24 +0530

    area.sh modification done

--- sequenceproblems.sh/area.sh
+++ sequenceproblems.sh/area.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -x
-echo "read length of rectangle"
-read  length
-echo "read bredth of rectangle"
-read bredth
-area=$(( $length * $bredth ))
-echo "total rectangular plot is:" $area
+echo "read the length of rectangle par feet"
+read a
+echo "read the breadth of rectangle par feet"
+read b
+c=$(($a * $b))
+echo "Area of plot in feet" $c
 metercon=0.092903
-meter=$(awk 'BEGIN {print}
+meter=$(awk 'BEGIN {print '$c' * '$metercon' * '25'}')
+echo "area of 25 plots in meter" $meter
+Sqmetertoacre=0.000247105
+acre=$(awk 'BEGIN {print '$meter' * '$Sqmetertoacre'}')
+echo "In acres" $acre
+
+
+
