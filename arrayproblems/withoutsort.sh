2e8dd3107309a0773e68cf41418c629ee0047a77
vattipallysushmitha <vengalreddysushmitha@gmail.com>
2022-07-08 17:49:26 +0530

    withoutsort,forloop modified

--- arrayproblems/withoutsort.sh
+++ arrayproblems/withoutsort.sh
@@ -1,21 +1,22 @@
-#!/bin/bash -x
-declare -a xyz
-for ((i=0; i<10; i++))
+declare -a vector
+vector=(11 21 12 34 4 665)
+echo "values are" ${vector[@]}
+max=${vector[0]}
+min=${vector[0]}
+
+for i in ${vector[@]}
 do
-	xyz[i]=$(($((RANDOM%999))+100))
-echo "array=" ${xyz[@]}
-done
-l1=0
-l2=0
-if [ ${xyz[i]} -gt $l1 ]
-then
-	l2=l1
-	l1=xyz[i]
-	echo "print 1st largest=" $l1
-	echo "print 2nd largest=" $l2	
-elif [ ${xyz[i]} -gt $l2 && ${xyz[i]} -lt $l1 ]
-then
-	l2=xyz[i]
-	echo "print 1st largest=" $l1
-	echo "print 2nd largest=" $l2
+
+if [[ $i -gt $max ]] ; then
+max=$i
 fi
+
+if [[ $i -lt $min ]] ; then
+min=$i
+
+fi
+done
+echo "Max value is $max, minimal value is $min"
+
+
+
