7d9a8e622329c5a4ae62398bbcc33b89a1a484b7
vattipallysushmitha <vengalreddysushmitha@gmail.com>
2022-07-09 15:18:13 +0530

    modified withoutsort

--- arrayproblems/withoutsort.sh
+++ arrayproblems/withoutsort.sh
@@ -1,22 +1,25 @@
 declare -a vector
 vector=(11 21 12 34 4 665)
 echo "values are" ${vector[@]}
-max=${vector[0]}
-min=${vector[0]}
+large=${vector[0]}
+small=${vector[0]}
 
 for i in ${vector[@]}
 do
 
-if [[ $i -gt $max ]] ; then
-max=$i
+if [[ $i -gt $large ]] 
+then
+	large=$i
 fi
 
-if [[ $i -lt $min ]] ; then
-min=$i
+if [[ $i -lt $small ]] 
+then
+	small=$i
 
 fi
 done
-echo "Max value is $max, minimal value is $min"
+echo "2nd large value is $large"
+echo "2nd small value is $small"
 
 
 
